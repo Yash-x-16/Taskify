08d24d0ae840f9ac8aa86c9cf3cd5821c93ae642
Yash_x_16 <mishrayash392@gmail.com>
2025-10-08 20:12:11 +0530

    made profile picture optional from zod validations

--- backend/src/validations/authValidations.ts
+++ backend/src/validations/authValidations.ts
@@ -4,10 +4,11 @@ export const signupValidation = z.object({
     username:z.string().min(6).max(30)  , 
     password:z.string().min(6) , 
     email:z.email() , 
-    profilePicture:z.string()
+    profilePicture:z.string().optional()
 })  
 
 export const SigninValidations = z.object({
     email:z.email() , 
     password:z.string().min(6)
-})
+}) 
+
