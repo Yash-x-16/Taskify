fcefd9e36f4ae5a2494d9db5617d6887bbca495b
Yash Mishra <mishrayash392@gmail.com>
2025-11-07 09:46:39 +0530

    rebuilding

--- backend/src/Routes/authRoutes.ts
+++
@@ -1,9 +0,0 @@
-import express from "express" 
-import { Signin, Signup } from "../Controller/authController.js";
-
-const router = express.Router() ; 
-
-router.post('/signup',Signup)
-router.post('/signin',Signin)
-
-export default router
